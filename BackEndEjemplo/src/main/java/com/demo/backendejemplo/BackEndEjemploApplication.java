43f675fbdc3abc8a899ff2ecdd92348392670880
Daniel García Estébanez <dgarciaestebanez@gmail.com>
2025-11-11 09:29:49 +0100

    asdf

---
+++ BackEndEjemplo/src/main/java/com/demo/backendejemplo/BackEndEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.demo.backendejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEjemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndEjemploApplication.class, args);
+    }
+
+}
---
+++ BackEndEjemplo/src/test/java/com/demo/backendejemplo/BackEndEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.backendejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEjemploApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
